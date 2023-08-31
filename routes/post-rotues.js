594cce1f3bae4959f44d537af41ea65e838d85b7
alyaxi <ali.zaman@quanrio.com>
2023-08-31 15:40:05 +0500

    add auth middleware in post routes

--- routes/post-rotues.js
+++ routes/post-rotues.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/", postController.createPost);
-router.get("/user/:user_id", postController.getPostsByUser);
+router.post("/",authMiddleware, postController.createPost);
+router.get("/user/:user_id",authMiddleware, postController.getPostsByUser);
 // Add more routes for updating and deleting posts if needed
 
 module.exports = router;
